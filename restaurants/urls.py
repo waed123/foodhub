d1f24fb9c0519f2d912ef53a63db55fff78bea42
Waed Jawarneh <waed@AUBs-MacBook-Pro.local>
2017-11-09 20:51:10 +0300

    bootstrap

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^list/$', views.restaurant_list , name="restaurant_list"),
     url(r'^detail/(?P<rest_id>\d+)/$', views.restaurant_detail , name="restaurant_detail"),
+
+    url(r'^restaurant_create/$', views.restaurant_create , name="restaurant_create"),
+    url(r'^restaurant_update/(?P<restaurant_id>\d+)/$', views.restaurant_update , name="restaurant_update"),
+    url(r'^restaurant_delete/(?P<restaurant_id>\d+)/$', views.restaurant_delete , name="restaurant_delete"),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
-# Create your views here.
+from .forms import RestaurantForm
+
+
+
 def restaurant_list(request):
 	context = {
 		"objects": models.Restaurant.objects.all()
@@ -13,3 +16,30 @@ def restaurant_detail(request, rest_id):
 		"object" : models.Restaurant.objects.get(id=rest_id)
 	}
 	return render(request, 'restaurant_detail.html', context)
+
+def restaurant_create(request):
+	form = RestaurantForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect("restaurant_list")
+	context = {
+		'form': form
+	}
+	return render(request, 'restaurant_create.html', context)
+
+def restaurant_update(request, restaurant_id):
+	item = models.Restaurant.objects.get(id=restaurant_id)
+	form = RestaurantForm(request.POST or None, instance = item)
+
+	if form.is_valid():
+		form.save()
+		return redirect("restaurant_detail", rest_id=restaurant_id)
+	context = {
+		'form': form,
+		"item": item,
+	}
+	return render (request, "restaurant_update.html", context)
+
+def restaurant_delete(request, restaurant_id):
+	item = models.Restaurant.objects.get(id=restaurant_id).delete()
+	return redirect("restaurant_list")
